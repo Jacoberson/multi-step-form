470845573295e5f1031076585a0b6d836141f02b
Jacoberson <c.jacob.moore92@gmail.com>
2023-11-20 14:04:41 -0600

    Create and position step navigator

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import StepNavigation from "./components/StepNavigation";
 import Step1 from "./components/Step1";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <StepNavigation />
       <Step1 />
     </>
   );
---
+++ src/components/StepNavigation.jsx
@@ -0,0 +1,12 @@
+import Button from "./Button";
+
+export default function StepNavigation() {
+  return (
+    <nav>
+      <Button type="button" text="1" />
+      <Button type="button" text="2" />
+      <Button type="button" text="3" />
+      <Button type="button" text="4" />
+    </nav>
+  );
+}
