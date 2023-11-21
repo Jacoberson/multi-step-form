afc6a5f0b721cbc5f643b8e6a9afd32052f58d85
Jacoberson <c.jacob.moore92@gmail.com>
2023-11-21 15:10:14 -0600

    Style step 2

--- src/components/navigationComponents/Step2.jsx
+++ src/components/navigationComponents/Step2.jsx
@@ -1,6 +1,9 @@
 import { PropTypes } from "prop-types";
 import Radio from "../htmlComponents/Radio";
 import Label from "../htmlComponents/Label";
+import ArcadeIcon from "../../assets/images/icon-arcade.svg";
+import AdvancedIcon from "../../assets/images/icon-advanced.svg";
+import ProIcon from "../../assets/images/icon-pro.svg";
 
 Step2.propTypes = {
   setCurrentStep: PropTypes.func,
@@ -12,33 +15,44 @@ export default function Step2() {
       <h1>Select your plan</h1>
       <p>You have the option of monthly or yearly billing.</p>
       <ul>
-        <li>
-          <h3>Arcade</h3>
-          <p>$9/mo</p>
+        <li className="plan current-selected">
+          <img src={ArcadeIcon} alt="" />
+          <div className="plan-text">
+            <h3>Arcade</h3>
+            <p>$9/mo</p>
+          </div>
         </li>
-        <li>
-          <h3>Advanced</h3>
-          <p>$12/mo</p>
+        <li className="plan">
+          <img src={AdvancedIcon} alt="" />
+          <div className="plan-text">
+            <h3>Advanced</h3>
+            <p>$12/mo</p>
+          </div>
         </li>
-        <li>
-          <h3>Pro</h3>
-          <p>$15/mo</p>
+        <li className="plan">
+          <img src={ProIcon} alt="" />
+          <div className="plan-text">
+            <h3>Pro</h3>
+            <p>$15/mo</p>
+          </div>
         </li>
       </ul>
-      <Label name="monthly" text="Monthly" />
-      <Radio
-        id="monthly"
-        name="billing"
-        value="monthly"
-        isDefaultChecked={true}
-      />
-      <Radio
-        id="yearly"
-        name="billing"
-        value="yearly"
-        isDefaultChecked={false}
-      />
-      <Label name="yearly" text="Yearly" />
+      <div className="billing-type">
+        <Label name="monthly" text="Monthly" />
+        <Radio
+          id="monthly"
+          name="billing"
+          value="monthly"
+          isDefaultChecked={true}
+        />
+        <Radio
+          id="yearly"
+          name="billing"
+          value="yearly"
+          isDefaultChecked={false}
+        />
+        <Label name="yearly" text="Yearly" />
+      </div>
     </div>
   );
 }
