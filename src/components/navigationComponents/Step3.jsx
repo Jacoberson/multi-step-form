1ff164ab2547d2573db56f2dc65553abd4f2525d
Jacoberson <c.jacob.moore92@gmail.com>
2023-11-23 19:53:11 -0600

    Add add-ons to json file and render from file

--- src/components/navigationComponents/Step3.jsx
+++ src/components/navigationComponents/Step3.jsx
@@ -1,5 +1,6 @@
 import { PropTypes } from "prop-types";
 import Checkbox from "../htmlComponents/Checkbox";
+import data from "../../data.json";
 
 Step3.propTypes = {
   isYearly: PropTypes.bool,
@@ -11,42 +12,20 @@ export default function Step3({ isYearly }) {
       <h1>Pick add-ons</h1>
       <p>Add-ons help enhance your gaming experience.</p>
       <ul>
-        <li className="add-on">
-          <Checkbox
-            id="online-service"
-            groupName="add-ons"
-            value="online-service"
-          />
-          <div className="add-on-text">
-            <h3>Online service</h3>
-            <p>Access to multiplayer games</p>
-          </div>
-          <p className="add-on-price">{isYearly ? "+$10/yr" : "+$1/mo"}</p>
-        </li>
-        <li className="add-on">
-          <Checkbox
-            id="larger-storage"
-            groupName="add-ons"
-            value="larger-storage"
-          />
-          <div className="add-on-text">
-            <h3>Larger storage</h3>
-            <p>Extra 1TB of cloud save</p>
-          </div>
-          <p className="add-on-price">{isYearly ? "+$20/yr" : "+$2/mo"}</p>
-        </li>
-        <li className="add-on">
-          <Checkbox
-            id="customize-profile"
-            groupName="add-ons"
-            value="customize-profile"
-          />
-          <div className="add-on-text">
-            <h3>Customizable profile</h3>
-            <p>Custom theme on your profile</p>
-          </div>
-          <p className="add-on-price">{isYearly ? "+$20/yr" : "+$2/mo"}</p>
-        </li>
+        {data["add-ons"].map(addOn => {
+          return (
+            <li key={addOn.name} className="add-on">
+              <Checkbox id={addOn.id} groupName="add-ons" value={addOn.id} />
+              <div className="add-on-text">
+                <h3>{addOn.name}</h3>
+                <p>{addOn.description}</p>
+              </div>
+              <p className="add-on-price">
+                {isYearly ? `+${addOn.yearlyPrice}` : `+${addOn.monthlyPrice}`}
+              </p>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
